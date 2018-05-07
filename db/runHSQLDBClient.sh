6c977b2f8807dec4bbd2bec6eeb976e3cffb1548
Krzysztof Kolecki <kolecki.krzysiek@gmail.com>
2018-05-07 17:44:04 +0200

    Travis configuration

--- db/runHSQLDBClient.sh
+++ db/runHSQLDBClient.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp ./hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/shopdb
+java -cp ./db/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/shopdb
--- db/runHSQLDBServer.sh
+++ db/runHSQLDBServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp ./hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 shopdb
+java -cp ./db/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 shopdb
